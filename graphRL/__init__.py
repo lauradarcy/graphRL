c39d714c17838a8b122773cb4689de8b2dbd68e6
luludarcy <luludarcy@gmail.com>
2018-12-14 19:45:27 +0000

    fix init files

--- graphRL/__init__.py
+++ graphRL/__init__.py
@@ -1,1 +1,7 @@
-name = "graphRL"
+from gym.envs.registration import register
+
+register(
+    id='graphRL-v0',
+    entry_point='graphRL.envs:graphRL',
+    kwargs={'network_size': 10, 'input_nodes': 3}
+)
--- graphRL/envs/__init__.py
+++ graphRL/envs/__init__.py
@@ -1,7 +1,1 @@
-from gym.envs.registration import register
-
-register(
-    id='MyEnv-v0',
-    entry_point='graphRL.graphRL:graphRL',
-    kwargs={'network_size': 10, 'input_nodes': 3}
-)
+from graphRL.envs.graphRL import graphRL
