139b14f74f54ee9dbe6fd95514954694c8acc39b
luludarcy <luludarcy@gmail.com>
2018-12-14 19:56:22 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='graphRL',
+      version='0.0.1',
+      author='Laura D\'Arcy',
+      author_email='luludarcy@gmail.com',
+      description='a directed acyclic graph environment for use with openAI gym',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/luludarcy/graphRL',
+      packages='setuptools.find_packages()',
+      install_requires=['gym'],  # And any other dependencies graphRL needs
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
